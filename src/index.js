91f38eea1a106d14595c49b5f1d1129e0a36a1bf
ddanailov <dimityr.danailov@gmail.com>
2020-07-15 15:56:49 +0300

    GiftList and Gift are the main components.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+import uuidv4 from 'uuid/v4'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {getInitialState, addGift, toggleReservation} from './gifts'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './misc/index.css'
+
+function Gift({gift, users, currentUser, onReserve}) {
+  return (
+    <div className={`gift ${gift.reservedBy ? 'reserved' : ''}`}>
+      <img src={gift.image} alt="gift" />
+      <div className="description">
+        <h2>{gift.description}</h2>
+      </div>
+      <div className="reservation">
+        {!gift.reservedBy || gift.reservedBy === currentUser.id ? (
+          <button onClick={() => onReserve(gift.id)}>
+            {gift.reservedBy ? 'Unreserve' : 'Reserve'}
+          </button>
+        ) : (
+          <span>{users[gift.reservedBy].name}</span>
+        )}
+      </div>
+    </div>
+  )
+}
+
+function GiftList() {
+  const [state, setState] = useState(() => getInitialState())
+  const {users, gifts, currentUser} = state
+
+  const handleAdd = () => {
+    const description = prompt('Gift to add')
+    if (description) {
+      setState(state =>
+        addGift(
+          state,
+          uuidv4(),
+          description,
+          'https://picsum.photos/200?q=' + Math.random(),
+        ),
+      )
+    }
+  }
+
+  const handleReserve = id => {
+    setState(state => toggleReservation(state, id))
+  }
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>H1, {currentUser.name}</h1>
+      </div>
+      <div className="actions">
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div className="gifts">
+        {gifts.map(gift => (
+          <Gift
+            key={gift.id}
+            gift={gift}
+            users={users}
+            currentUser={currentUser}
+            onReserve={handleReserve}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<GiftList />, document.getElementById('root'))
